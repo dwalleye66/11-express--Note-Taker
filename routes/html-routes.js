e73bd3cdbdacdd763cc527c081f5be13d3312e2d
Darin Wallace <dwalleye66@gmail.com>
2020-12-01 18:27:42 -0600

    updated files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,29 @@
-Testing
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require('path');
+var router = require('express').Router();
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+// HTML GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases the user is shown an HTML page of content
+// ---------------------------------------------------------------------------
+
+router.get('/tables', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../public/tables.html'));
+});
+
+router.get('/reserve', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../public/reserve.html'));
+});
+
+// If no matching route is found default to home
+router.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../public/home.html'));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-Testing 
+const express = require("express");
+const routes = require("routes");
+
+//server app at port 3306
+const app = express();
+const PORT = process.env.PORT || 3306;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// js files required
+app.use("./routes/api-routes")(app);
+app.use("./routes/html-routes")(app);
+
+// use public folder files
+app.use(express.static("public"));
+
+//add listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT; " + PORT);
+});
+
