3275e6042fe96dd1b97c8663bf7cbeb2434c91aa
Darin Wallace <dwalleye66@gmail.com>
2020-12-03 09:12:53 -0600

    fixed routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,44 @@
-Test 
+//Load Data
+const fs = require('fs')
+
+//Get notes
+module.exports = function (app) {
+    app.get('api/notes', function (req, res) {
+        fs.readFile('./db/db/json', 'utf8', (err, data) => {
+            if (err) throw err;
+            res.json(JSON.parse(data));
+        });
+    });
+
+    //Read notes
+    app.post('./db/db.json', 'utf', (err, data) => {
+        if (err) throw err;
+        let newNote = JSON.parse(data);
+        let id = (newNote.length) + 1;
+        let newNoteBody = JSON.stringify(req.body);
+        newNoteBody = JSON.parse(newNoteBody);
+        newNote.push({ ...newNoteBody, "id": id });
+        newNote = JSON.stringify(newNote);
+        fs.writeFile('./db/db.json', newNote, (err, data) => {
+            res.sendStatus(200);
+        });
+    });
+
+    //Delete notes
+    app.delete("/api/notes/:id", function (req, res) {
+        let id = req.params.id;
+        console.log(id);
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            let deleteNote = JSON.parse(data);
+            console.log(deleteNote);
+            deleteNote = deleteNote.filter(notes => notes.id != id);
+            console.log(deleteNote);
+            deleteNote = JSON.stringify(deleteNote);
+            fs.writeFile('./db/db.json', deleteNote, (err, data) => {
+                if (err) throw err;
+                res.send(deleteNote);
+            });
+        });
+    });
+};
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,16 @@
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
-var path = require('path');
-var router = require('express').Router();
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+var path = require("path");
 
-// HTML GET Requests
-// Below code handles when users "visit" a page.
-// In each of the below cases the user is shown an HTML page of content
-// ---------------------------------------------------------------------------
-
-router.get('/tables', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/tables.html'));
-});
-
-router.get('/reserve', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/reserve.html'));
-});
-
-// If no matching route is found default to home
-router.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/home.html'));
-});
-
-module.exports = router;
+//Get notes
+module.exports = function(app) {
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  //Default path
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+;}
